53cc4ca7ecd2167ccfd48e98bec0a20b4edc4e84
MatiasOjman <167230970+MatiasOjman@users.noreply.github.com>
2024-08-02 14:54:32 -0300

    hola

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-document.querySelector('form').addEventListener('submit', function (e) {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+// document.querySelector('form').addEventListener('submit', function (e) {
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
 
-    if (username === '' || password === '') {
-        alert('Por favor, complete todos los campos.');
-        e.preventDefault();
-    }
-});
+//     if (username === '' || password === '') {
+//         alert('Por favor, complete todos los campos.');
+//         e.preventDefault();
+//     }
+// });
