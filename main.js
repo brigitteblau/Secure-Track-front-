e2e26c0d60f0cd07c3846ad8ece12272ad1a3310
userbrigu <brigitteyaelblau@gmail.com>
2024-08-04 12:12:44 -0300

    zzz

--- main.js
+++ main.js
@@ -11,3 +11,28 @@ document.querySelector('form').addEventListener('submit', function (e) {
         location.href = "selectorItems.html"
     }
 });
+
+//acordion 
+document.addEventListener('DOMContentLoaded', () => {
+    const accordionButtons = document.querySelectorAll('.accordion-button');
+    const floorsButtons = document.querySelectorAll('.floors-button');
+
+    accordionButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const content = button.nextElementSibling;
+            content.classList.toggle('active');
+            button.querySelector('.arrow').textContent = content.classList.contains('active') ? '▲' : '▼';
+        });
+    });
+
+    floorsButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const content = button.nextElementSibling;
+            if (content) {
+                content.classList.toggle('active');
+                button.querySelector('.arrow').textContent = content.classList.contains('active') ? '▲' : '▼';
+            }
+        });
+    });
+});
+
